21307482e15573c96f7e199b665039e8b285d60e
WelcomeToTheOblivion <cjp.vicerra@gmail.com>
2019-10-11 16:00:06 +0000

    modified Questionnaire for games 2 and 3

--- src/psych1/pkg1/Questionnaire.java
+++ src/psych1/pkg1/Questionnaire.java
@@ -7,15 +7,25 @@ public class Questionnaire{
     String[] questions;
     String[] correctAns;
     String[] wrongAns;
+
+    //prevents asking questions twice
     ArrayList<Integer> usedQuestions = new ArrayList<Integer>();
 
+    //for Game 1
     public Questionnaire(String[] questions, String[] correctAns, String[] wrongAns){
         this.questions = questions;
         this.correctAns = correctAns;
         this.wrongAns = wrongAns;
     }
 
+    //for Game 2 and 3
+    public Questionnaire(String[] questions, String[] correctAns){
+        this.questions = questions;
+        this.correctAns = correctAns;
+    }
+
     private String getQuestion(){
+        //randomizes what question to get
         int random = (int)(Math.random() * questions.length);
         String question;
         if(usedQuestions.contains(random)){
@@ -24,13 +34,15 @@ public class Questionnaire{
         else{
             question = questions[random];
         }
+        //adds index number to store numbers used
         usedQuestions.add(random);
         return question;
     }
 
     private String[] getChoices(int i){
-        int random = (int)(Math.random() * 2);
         String[] choices;
+        //Simplicity's sake; so that all answers are in one side for Game 1
+        int random = (int)(Math.random() * 2);
         if(random == 0){
             choices = new String[]{correctAns[i], wrongAns[i]};
         }
