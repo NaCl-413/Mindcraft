efe9ebc0db61af81e6e2282077fe7f0af3748863
WelcomeToTheOblivion <cjp.vicerra@gmail.com>
2019-10-11 15:49:21 +0000

    add methods in questionnaire

--- src/psych1/pkg1/Questionnaire.java
+++ src/psych1/pkg1/Questionnaire.java
@@ -1,10 +1,13 @@
 package psych1.pkg1;
 
-public class Questionnaire extends {
+import java.util.ArrayList;
+
+public class Questionnaire{
 
     String[] questions;
     String[] correctAns;
     String[] wrongAns;
+    ArrayList<Integer> usedQuestions = new ArrayList<Integer>();
 
     public Questionnaire(String[] questions, String[] correctAns, String[] wrongAns){
         this.questions = questions;
@@ -12,4 +15,32 @@ public class Questionnaire extends {
         this.wrongAns = wrongAns;
     }
 
+    private String getQuestion(){
+        int random = (int)(Math.random() * questions.length);
+        String question;
+        if(usedQuestions.contains(random)){
+            question = getQuestion();
+        }
+        else{
+            question = questions[random];
+        }
+        usedQuestions.add(random);
+        return question;
+    }
+
+    private String[] getChoices(int i){
+        int random = (int)(Math.random() * 2);
+        String[] choices;
+        if(random == 0){
+            choices = new String[]{correctAns[i], wrongAns[i]};
+        }
+        else{
+            choices = new String[]{wrongAns[i], correctAns[i]};
+        }
+        return choices;
+    }
+
+    private String getAnswer(int i){
+        return correctAns[i];
+    }
 }
