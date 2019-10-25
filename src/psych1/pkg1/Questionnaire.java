d4fd00b374bcdf2b0af9c9add21e916c3368533c
WelcomeToTheOblivion <51913199+WelcomeToOblivion@users.noreply.github.com>
2019-10-26 01:28:31 +0800

    optimized methods

--- src/psych1/pkg1/Questionnaire.java
+++ src/psych1/pkg1/Questionnaire.java
@@ -10,8 +10,9 @@ public class Questionnaire{
     private String[] correctAns;
     private String[] wrongAns;
     
-    ArrayList<Integer> correctAnsIndex = new ArrayList<Integer>();
+    ArrayList<Integer> correctChoiceIndex = new ArrayList<Integer>();
     ArrayList<Integer> questionsSelected = new ArrayList<Integer>();
+    ArrayList<String[]> questionsChoices = new ArrayList<String[]>();
 
     public Questionnaire(String[] questions, String[] correctAns, String[] wrongAns){
         this.questions = questions;
@@ -19,10 +20,11 @@ public class Questionnaire{
         this.wrongAns = wrongAns;
     }
     
+//Framework Methods
     public String getQuestion(int i){
         return questions[i];
     }
-    
+
     public String getCorrectAnswer(int i){
         return correctAns[i];
     }
@@ -31,29 +33,28 @@ public class Questionnaire{
         return wrongAns[i];
     }
     
-    public int getCorrectAnswerIndex(){
-        return correctAnsIndex.get(0);
-    }
-    
-    public int getCurrentQuestionIndex(){
+//Methods to ease code linespace
+    public int getCurQuestionIndex(){
         return questionsSelected.get(0);
     }
     
-    public String[] getChoices(int i){
-        String[] choices;
-        //Simplicity's sake; so that all answers are in one side for Game 1
-        int random = (int)(Math.random() * 2);
-        if(random == 0){
-            choices = new String[]{correctAns[i], wrongAns[i]};
-            correctAnsIndex.add(1);
-        }
-        else{
-            choices = new String[]{wrongAns[i], correctAns[i]};
-            correctAnsIndex.add(2);
-        }
-        return choices;
+    public String[] getCurChoices(){
+        return questionsChoices.get(0);
     }
     
+    public String getCurQuestion(){
+        return getQuestion(getCurQuestionIndex());
+    }
+    
+    public String getCurCorrectAns(){
+        return getCorrectAnswer(getCurQuestionIndex());
+    }
+    
+    public int getCurCorrectChoiceIndex(){
+        return correctChoiceIndex.get(0);
+    }
+    
+//Init and in-game Methods
     public void resetQuestionnaire(){
         questionsSelected.clear();
         for(int i = 0;i < numQuestions;i++){
@@ -61,26 +62,41 @@ public class Questionnaire{
         }
     }
 
-    public String selectQuestion(){
-        //randomizes what question to get
-        int random = (int)(Math.random() * questions.length);
-        String question;
-        //prevents adding a question twice
-        if(questionsSelected.contains(random)){
-            question = selectQuestion();
+    public void selectChoices(int i){
+        String[] choices;
+        //Simplicity's sake; so that all answers are in one side for Game 1
+        int random = (int)(Math.random() * 2);
+        if(random == 0){
+            choices = new String[]{correctAns[i], wrongAns[i]};
+            correctChoiceIndex.add(1);
         }
         else{
-            question = questions[random];
+            choices = new String[]{wrongAns[i], correctAns[i]};
+            correctChoiceIndex.add(2);
         }
-        //adds index number to questionSelected
-        questionsSelected.add(random);
-        return question;
+        questionsChoices.add(choices);
+    }
+
+    public int selectQuestion(){
+        //randomizes what question to get
+        int qNum = (int)(Math.random() * questions.length);
+        
+        //prevents adding a question twice/ runs algorithm again
+        if(questionsSelected.contains(qNum)){
+            qNum = selectQuestion();
+        }else{
+            //adds index number to questionSelected
+            questionsSelected.add(qNum);
+            //selects choices(for game 1)
+            selectChoices(qNum);
+        }
+        return qNum;
     }
     
     public boolean nextQuestion(){
         if(!questionsSelected.isEmpty()){
             questionsSelected.remove(0);
-            correctAnsIndex.remove(0);
+            correctChoiceIndex.remove(0);
         }
         return !questionsSelected.isEmpty();
     }
