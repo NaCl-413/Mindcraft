457ef95390a059f927f123c586e9e80c2df50033
NaCl-413 <gavinnigelchuacuco@gmail.com>
2019-10-26 18:40:43 +0800

    Added gameEnd(), started work on updateQuestionPanel()

--- src/psych1/pkg1/Mindcraft.java
+++ src/psych1/pkg1/Mindcraft.java
@@ -4,7 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -12,20 +12,22 @@ import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 public class Mindcraft extends JFrame implements ActionListener, KeyListener{
     
-    private String q1File = "questionnaire1.txt";
-    private String q2File = "questionnaire2.txt";
-    private String q3File = "questionnaire3.txt";
+    private String q1File = "questionnaires\\questionnaire1.txt";
+    private String q2File = "questionnaires\\questionnaire2.txt";
+    private String q3File = "questionnaires\\questionnaire3.txt";
     
     private ImageIcon aboutImage = new ImageIcon("C:\\Users\\Gavin Nigel Chuacuco\\Desktop\\Desktop\\Screenshots\\longverticalpicforjava.jpg");
     private ImageIcon instructionsImage = new ImageIcon("C:\\Users\\Gavin Nigel Chuacuco\\Desktop\\Desktop\\Screenshots\\longverticalpicforjava.jpg");
@@ -48,16 +50,18 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
     private JScrollPane aboutPane = new JScrollPane(new JLabel(aboutImage));
     
     private JButton toMainMenu = new JButton("MAIN MENU");
-    private JButton toGame = new JButton("START");
+    private JButton toGame = new JButton("PLAY");
     private JButton toAbout = new JButton("ABOUT");
     private JButton toInstructions = new JButton("INSTRUCTIONS");
     private JButton toReviewer = new JButton("REVIEWER");
-        private JButton startGame = new JButton("START GAME");  //Tentative start button for the game
+    private JButton startGameB = new JButton("START GAME");  //Tentative start button for the game
 
     private JLabel mainTitle = new JLabel("MindCraft", JLabel.CENTER);
     private JLabel aboutTitle = new JLabel("ABOUT", JLabel.CENTER);
     private JLabel instructionsTitle = new JLabel("INSTRUCTIONS", JLabel.CENTER);
     private JLabel reviewerTitle = new JLabel("REVIEWER", JLabel.CENTER);
+    
+    private Font f = new Font("Helvetica",Font.BOLD,25);
     
     private JLabel game1Title = new JLabel("GAME 1", JLabel.CENTER);
     private JLabel game1Question = new JLabel("QUESTION: ", JLabel.CENTER);
@@ -69,6 +73,7 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
     private JLabel game2UserInput = new JLabel("WADS", JLabel.CENTER);
     
     private JLabel game3Title = new JLabel("GAME 3", JLabel.CENTER);
+    private JLabel game3Shape = new JLabel("SHAPE", JLabel.CENTER);
     private JLabel gameTimer = new JLabel("Time (00 : 00)", JLabel.CENTER);
         
     
@@ -83,6 +88,7 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
     private boolean isG3End = false;
     private int wrongAnsCount;
     private int finalTime;
+    private int finalScore;
     
     public Mindcraft(){
         super("MINDCRAFT");
@@ -91,15 +97,16 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
     //Panel Initializations(Init)
     //Title Panel
         titlePanel.setPreferredSize(new Dimension(590, 720));
-        titlePanel.setBackground(Color.cyan);
+        titlePanel.setBackground(Color.gray);
         titlePanel.setLayout(new BorderLayout());
+        mainTitle.setIcon(new ImageIcon("panelImage\\testTitlePanel.jpg"));
         titlePanel.add(mainTitle, BorderLayout.CENTER);
         
     //Main Menu Panel
         mainMenuPanel.setPreferredSize(new Dimension(590, 720));
         mainMenuPanel.setBackground(Color.gray);
         mainMenuPanel.setLayout(new BoxLayout(mainMenuPanel, BoxLayout.Y_AXIS));
-        mainMenuPanel.add(Box.createRigidArea(new Dimension(0,65)));
+        mainMenuPanel.add(Box.createRigidArea(new Dimension(0,50)));
         mainMenuPanel.add(toGame);
         mainMenuPanel.add(Box.createRigidArea(new Dimension(0,130)));
         mainMenuPanel.add(toAbout);
@@ -143,13 +150,15 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
         gameInterface.setPreferredSize(new Dimension(1250, 30));
         gameInterface.setLayout(new BorderLayout());  
         gameInterface.add(gameTimer, BorderLayout.CENTER);
-        gameInterface.add(startGame, BorderLayout.WEST);
+        gameInterface.add(startGameB, BorderLayout.WEST);
         
     //Game1 Panel
         game1Panel.setPreferredSize(new Dimension(1250, 310));
         game1Panel.setLayout(new BorderLayout());
         game1Panel.add(game1Title, BorderLayout.CENTER);
         game1Panel.add(game1Question, BorderLayout.SOUTH);
+        game1Question.setFont (f);
+        System.out.println(game1Question.getFont ());
         game1Panel.add(game1OptionR, BorderLayout.EAST);
         game1Panel.add(game1OptionL, BorderLayout.WEST);
        /*
@@ -169,7 +178,8 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
     //Game3 Panel
         game3Panel.setPreferredSize(new Dimension(625, 310));
         game3Panel.setLayout(new BorderLayout());
-        game3Panel.add(game3Title, BorderLayout.CENTER);
+        game3Panel.add(game3Title, BorderLayout.NORTH);
+        game3Panel.add(game3Shape,BorderLayout.CENTER);
         /*
             DITO MO LAGAY COMPONENTS NG GAME 3        
         */
@@ -181,7 +191,7 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
         gamePanel.add(game1Panel, BorderLayout.NORTH);
         gamePanel.add(game2Panel, BorderLayout.WEST);
         gamePanel.add(game3Panel, BorderLayout.EAST);
-
+        
     //Buttons Init
         toMainMenu.addActionListener(this);
         toGame.addActionListener(this);
@@ -194,15 +204,15 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
         g1Questionnaire = tempArray[0];
         g2Questionnaire = tempArray[1];
         g3Questionnaire = tempArray[2];
-
+        
     //Addition of Components
         //separates titlePanel(Left) and main content called rightPanel(Right)
         rightPanel = mainMenuPanel;
         add(titlePanel, BorderLayout.WEST);
         add(rightPanel, BorderLayout.CENTER);
-        
+         
     }
-    
+        
     private Questionnaire[] questionnaireInit(){
         ArrayList<Questionnaire> masterList = new ArrayList<Questionnaire>();
 
@@ -214,7 +224,7 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
         ArrayList<String> correctAnsList;
         ArrayList<String> wrongAnsList;
         String[] split;
-            
+        
         try {
             for(String s: fileList){
                 questionList = new ArrayList<String>();
@@ -229,6 +239,7 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
                     questionList.add(split[0]);
                     correctAnsList.add(split[1]);
                     wrongAnsList.add(split[2]);
+                    System.out.println(split[0] + "  " + split[1] + "  " + split[2]);
                 }
                 //data structure change: ArrayList to Array
                 String[] tempQArray = new String[questionList.size()];
@@ -241,11 +252,12 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
                 //adds Questionnaire to arraylist
                 masterList.add(new Questionnaire(tempQArray,tempCArray,tempWArray));
                 reader.close();
+                 
             }
         }catch(Exception e){
             System.out.println(e);
         }
-
+        
         Questionnaire[] tempMArray = new Questionnaire[masterList.size()];
         tempMArray = masterList.toArray(tempMArray);
         
@@ -258,6 +270,8 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
         int startTime = (int)System.currentTimeMillis()/1000;
         int prevTime = startTime;
         int currentTime;
+        updateQuestionPanel(4);
+        
         /*
         while(!isEnd){
             currentTime = (int)System.currentTimeMillis()/1000;
@@ -279,14 +293,54 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
         g2Questionnaire.resetQuestionnaire();
         g3Questionnaire.resetQuestionnaire();
     }
-    
-    private void updateQuestionPanel(){
-        /*
-            DITO MO LAGAY PAGPALIT NG QUESTION                                          
-        */
+    //To indicate that the games have been finished
+    private void gameEnd(int game){
+        
+        if(game == 1){
+        game1Title.setText("GAME 1 FINISHED");
+        game1Question.setText("GAME 1 FINISHED");
+        game1OptionR.setText("GAME 1 FINISHED");
+        game1OptionL.setText("GAME 1 FINISHED");
+        }else if (game == 2){
+        game2Pattern.setText("GAME 2 FINISHED");
+        game2UserInput.setText("GAME 2 FINISHED");
+        game2Title.setText("GAME 2 FINISHED");
+        }else if (game == 3){
+        game3Shape.setText("GAME 3 FINISHED");
+        game3Title.setText("GAME 3 FINISHED");
+        }else if (game == 4){  //Means that the whole game has ended
+            wrongAnsCount = wrongAnsCount*5; //5 is our mistake-score multiplier
+            finalScore = finalTime; /* Still not sure if finalTime will be minutes:seconds or just seconds. So both score and finalTime will be the same for now*/
+            finalScore = finalScore - wrongAnsCount;
+            JFrame f = new JFrame();
+            JOptionPane.showMessageDialog(f,"GAME FINISHED!\n\n" + "Time: " + finalTime + "\nMistake Penalty: "+ wrongAnsCount + "\nYour Score: " + finalScore, "GAME FINISHED!",1);  
+        }
+        
     }
     
+    
+    //Gets the next question and displays it
+    private void updateQuestionPanel(int game){
+        
+        
+        if(game == 1){
+        game1Question.setText("<html>Question:  "+ g1Questionnaire.getCurQuestion() +"<br><br/>"+"</html>");
+        }else if (game == 2){
+        game2Pattern.setText(g2Questionnaire.getCurQuestion());
+        }else if (game == 3){
+        game3Shape.setText(g3Questionnaire.getCurQuestion());
+        }else if (game == 4){  //Means that it will need to update all 3 games
+            game1Question.setText("<html>Question:  "+ g1Questionnaire.getCurQuestion() +"<br><br/>"+"</html>");
+            game2Pattern.setText(g2Questionnaire.getCurQuestion());
+            game3Shape.setText(g3Questionnaire.getCurQuestion());
+        }
+        
+    }
+    
+
+    
     private boolean checkAnswer(int i, int a){
+        
         boolean correct = false;
         if(i == 1){
             if(a == g1Questionnaire.getCurCorrectChoiceIndex()){
@@ -314,7 +368,7 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
         System.out.println("reached actionPerformed");
         JButton clicked = (JButton)a.getSource();
         remove(rightPanel);
-
+        
         
         if(clicked == toGame){
             isInGame = true;
@@ -331,6 +385,7 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
             }else{
                 titlePanel.setPreferredSize(new Dimension(400, 300));
                 if(clicked == toAbout){
+                    gameEnd(4);
                     rightPanel = aboutPanel;
                     aboutPanel.add(toMainMenu, BorderLayout.PAGE_END);
                 }else if(clicked == toInstructions){
@@ -354,7 +409,7 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
         int gameNumber = 0;
         int keyNumber = 0;
         int key = e.getKeyCode();
-                                              
+                                      
         if(key == KeyEvent.VK_LEFT){
             isKeyCorrect = true;
             keyNumber = 1;
@@ -379,21 +434,25 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
             isKeyCorrect = true;
             keyNumber = 1;
             gameNumber = 3;
+            game2UserInput.setText(game2UserInput.getText()+"W");
         }
         if(key == KeyEvent.VK_A){
             isKeyCorrect = true;
             keyNumber = 2;
             gameNumber = 3;
+            game2UserInput.setText(game2UserInput.getText()+"A");
         }
         if(key == KeyEvent.VK_S){
             isKeyCorrect = true;
             keyNumber = 3;
             gameNumber = 3;
+            game2UserInput.setText(game2UserInput.getText()+"S");
         }
         if(key == KeyEvent.VK_D){
             isKeyCorrect = true;
             keyNumber = 4;
             gameNumber = 3;
+            game2UserInput.setText(game2UserInput.getText()+"D");
         }
         if(isKeyCorrect){
             boolean correct = false;
@@ -403,22 +462,25 @@ public class Mindcraft extends JFrame implements ActionListener, KeyListener{
                 if(gameNumber == 1){
                     if(!g1Questionnaire.nextQuestion()){
                         isG1End = true;
+                        updateQuestionPanel(1);
                     }
                 }else if(gameNumber == 2){
                     if(!g2Questionnaire.nextQuestion()){
                         isG2End = true;
+                        updateQuestionPanel(2);
                     }
                 }else if(gameNumber == 3){
                     if(!g3Questionnaire.nextQuestion()){
                         isG3End = true;
+                        updateQuestionPanel(3);
                     }
                 }
                 
-                updateQuestionPanel();
+                
             }
         }
     }
-
+    
     @Override
     public void keyTyped(KeyEvent e) {
         try{}catch(Exception ex){
